3ff092038094e1151844d7827fff9a46514473f6
AJD <adriana.j.didden@gmail.com>
2020-02-13 10:03:59 -0500

    got ajax function working to show city searched

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 
 $("#find-city").on("click", function (event) {
-
-    // event.preventDefault() can be used to prevent an event's default behavior.
-    // Here, it prevents the submit button from trying to submit a form when clicked
     event.preventDefault();
 
-    // Here we grab the text from the input box
     var city = $("#city-input").val();
 
     var APIKey = "8e9ec42762f02540fd0a774a11d58c6b";
 
-    var queryURL ="api.openweathermap.org/data/2.5/weather?q="+city+"&appid=8e9ec42762f02540fd0a774a11d58c6b";
-    
+    var queryURL ="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=8e9ec42762f02540fd0a774a11d58c6b";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        $("#city-view").text(JSON.stringify(response));
+        $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+        $(".temp").text("Temperature (F) " + response.main.temp);
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".wind").text("Wind Speed: " + response.wind.speed);
+
+
+    });
     
 });
